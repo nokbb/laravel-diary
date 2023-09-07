5f6a80278dde9c5e7b8aebbdd73f518af33ac5fd
Your Name <you@example.com>
2023-09-07 15:04:04 +0900

    create account lock

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class LoginController extends Controller
@@ -28,10 +29,38 @@ class LoginController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        //①アカウントがロックされていたら弾く
+        $user = User::where('email', '=', $credentials['email'])->first();
 
-            return redirect()->route('home')->with('success', 'ログイン成功しました！');
+        if (!is_null($user)) {
+            if ($user->locked_flg === 1) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);
+            }
+
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                //②成功したらエラーカウントを0にする
+                if ($user->error_count > 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+
+                return redirect()->route('home')->with('success', 'ログイン成功しました！');
+            }
+
+            //③ログイン失敗したらエラーカウントを1増やす
+            $user->error_count = $user->error_count + 1;
+            //④エラーカウントが6以上の場合はアカウントをロックする
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                $user->save();
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除したい場合は運営者に連絡してください。',
+                ]);
+            }
+            $user->save();
         }
 
         return back()->withErrors([
